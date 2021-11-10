f0be4093e6a8c5147df99653d247aaf993f0523f
vinhasdf <vinhasdf12@gmail.com>
2021-11-10 15:10:05 +0700

    First commit

---
+++ binance_project/coins.py
@@ -0,0 +1,116 @@
+import requests
+
+# get close price of 1 coin with url, return with list
+class coin:
+    def __init__(self, name):
+        url = "https://api.binance.com/api/v3/klines?interval=1d&limit=270&symbol=" + name
+        r = requests.get(url)
+        self.coin_prices = []
+        # 15 array
+        if r.status_code != 200:
+            return
+        info = r.json()
+
+        if (len(info) >= 15):
+            for i in range(0, len(info)):
+                self.coin_prices.append(float(info[i][4]))
+        
+    def getlist(self):
+        return self.coin_prices
+
+class coinlist:
+    def __init__(self):
+        r = requests.get("https://api.binance.com/api/v3/exchangeInfo")
+        self.coinlist = []
+
+        if r.status_code != 200:
+            return
+
+        info = r.json()
+        pairs = info["symbols"]
+
+        for item in pairs:
+            tempstr = item['symbol']
+            status = item['status']
+            if (tempstr.find("USDT") > 0 and status == 'TRADING'):
+            # if (status == 'TRADING'):
+                # add to coin list
+                self.coinlist.append(tempstr)
+    
+    # return coinlist
+    def getcoinlist(self):
+        return self.coinlist
+
+# calculate rsi 14 of all days
+def calrsi14(price_list):
+    ag = 0
+    al = 0
+
+    # first 15 day
+    for i in range(1, 15):
+        if ((price_list[i] - price_list[i - 1]) > 0):
+            ag += (price_list[i] - price_list[i - 1])
+        elif ((price_list[i - 1] - price_list[i]) > 0):
+            al += (price_list[i - 1] - price_list[i])
+    
+    ag /= 14
+    al /= 14
+
+    # use prior value to calculate remain ag and al
+    for i in range(15, len(price_list)):
+        if ((price_list[i] - price_list[i - 1]) > 0):
+            ag = ((ag * 13) + (price_list[i] - price_list[i - 1])) / 14
+            al = (al * 13) / 14
+        elif ((price_list[i - 1] - price_list[i]) > 0):
+            al = ((al * 13) + (price_list[i - 1] - price_list[i])) / 14
+            ag = (ag * 13) / 14
+        else:
+            al = (al * 13) / 14
+            ag = (ag * 13) / 14
+    
+    if (al == 0):
+        return 100.0
+
+    return 100 - 100/(1 + ag/al)
+
+# calculate rsi 14 of only 14 last days
+def calrsi14_ver2(price_list):
+    ag = 0
+    al = 0
+
+    # first 15 day
+    for i in range((len(price_list) - 14), len(price_list)):
+        if ((price_list[i] - price_list[i - 1]) > 0):
+            ag += (price_list[i] - price_list[i - 1])
+        elif ((price_list[i - 1] - price_list[i]) > 0):
+            al += (price_list[i - 1] - price_list[i])
+    
+    # ag /= 14
+    # al /= 14
+    
+    if (al == 0):
+        return 100.0
+
+    return 100 - 100/(1 + ag/al)
+
+fw = open("temp.txt", "w")
+
+coins = coinlist()
+lines = coins.getcoinlist()
+
+for nline in lines:
+    testlist = coin(nline)
+    # store all coin to dictionary and sort by rsi
+    if (len(testlist.getlist()) > 0):
+        tempstr = (str(nline) + '\t' + str(calrsi14(testlist.getlist())) + 
+        '\t' + str(calrsi14_ver2(testlist.getlist())) + 
+        '\t' + str(len(testlist.getlist())) + '\n')
+        tempstr = tempstr.replace('.', ',')
+        print(tempstr)
+        fw.write(tempstr)
+
+fw.close()
+
+# print(len(mydic))
+# for line in sorted(mydic.items(), key = lambda kv:(kv[1], kv[0])):
+#     print(line)
---
+++ gateio/getcoinlist.py
@@ -0,0 +1,112 @@
+import requests
+
+# get close price of 1 coin with url, return with list
+class coin:
+    def __init__(self, name):
+        url = "https://data.gateapi.io/api2/1/candlestick2/" + name + "?group_sec=86400&range_hour=6456"
+        r = requests.get(url)
+        self.coin_prices = []
+        # 15 array
+        if r.status_code != 200:
+            return
+        info = r.json()["data"]
+
+        if (len(info) >= 15):
+            for i in range(0, len(info)):
+                self.coin_prices.append(float(info[i][2]))
+        
+    def getlist(self):
+        return self.coin_prices
+
+class coinlist:
+    def __init__(self):
+        r = requests.get("https://data.gateapi.io/api2/1/marketinfo")
+        self.coinlist = []
+
+        if r.status_code != 200:
+            return
+
+        info = r.json()
+        pairs = info["pairs"]
+
+        for item in pairs:
+            for key, value in item.items():
+                tempstr = str(key)
+                if (tempstr.find("_usdt") != -1):
+                    # add to coin list
+                    self.coinlist.append(tempstr)
+                break
+    
+    # return coinlist
+    def getcoinlist(self):
+        return self.coinlist
+
+# calculate rsi 14 of all days
+def calrsi14(price_list):
+    ag = 0
+    al = 0
+
+    # first 15 day
+    for i in range(1, 15):
+        if ((price_list[i] - price_list[i - 1]) > 0):
+            ag += (price_list[i] - price_list[i - 1])
+        elif ((price_list[i - 1] - price_list[i]) > 0):
+            al += (price_list[i - 1] - price_list[i])
+    
+    ag /= 14
+    al /= 14
+
+    # use prior value to calculate remain ag and al
+    for i in range(15, len(price_list)):
+        if ((price_list[i] - price_list[i - 1]) > 0):
+            ag = ((ag * 13) + (price_list[i] - price_list[i - 1])) / 14
+            al = (al * 13) / 14
+        elif ((price_list[i - 1] - price_list[i]) > 0):
+            al = ((al * 13) + (price_list[i - 1] - price_list[i])) / 14
+            ag = (ag * 13) / 14
+        else:
+            al = (al * 13) / 14
+            ag = (ag * 13) / 14
+    
+    if (al == 0):
+        return 100.0
+
+    return 100 - 100/(1 + ag/al)
+
+# calculate rsi 14 of only 14 last days
+def calrsi14_ver2(price_list):
+    ag = 0
+    al = 0
+
+    # first 15 day
+    for i in range((len(price_list) - 14), len(price_list)):
+        if ((price_list[i] - price_list[i - 1]) > 0):
+            ag += (price_list[i] - price_list[i - 1])
+        elif ((price_list[i - 1] - price_list[i]) > 0):
+            al += (price_list[i - 1] - price_list[i])
+    
+    # ag /= 14
+    # al /= 14
+    
+    if (al == 0):
+        return 100.0
+
+    return 100 - 100/(1 + ag/al)
+
+fw = open("temp.txt", "w")
+
+coins = coinlist()
+lines = coins.getcoinlist()
+
+for nline in lines:
+    testlist = coin(nline)
+    # store all coin to dictionary and sort by rsi
+    if (len(testlist.getlist()) > 0):
+        tempstr = (str(nline) + '\t' + str(calrsi14(testlist.getlist())) + 
+        '\t' + str(calrsi14_ver2(testlist.getlist())) + 
+        '\t' + str(len(testlist.getlist())) + '\n')
+        tempstr = tempstr.replace('.', ',')
+        print(tempstr)
+        fw.write(tempstr)
+
+fw.close()
